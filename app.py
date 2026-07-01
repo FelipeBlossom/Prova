6f9168f025858b9b920dfc120c3e81b58d2eb7bb
FelipeBlossom <flor.felipe@academico.ifpb.edu.br>
2026-07-01 20:08:57 -0300

    fix: corrigir raiz e adicionar para raiz

--- app.py
+++ app.py
@@ -11,37 +11,40 @@ FILMES = {
 }
 app.config['FILMES'] = FILMES
 
-def obter_filmes():
-    return session.get('filmes', [])
-
-def adicionar_filme(titulo):
-    filmes = obter_filmes()
-    filmes.append(titulo)
-    session['filmes'] = filmes
-
-def remover_filme(indice):
-    filmes = obter_filmes()
-    if 0 <= indice < len(filmes):
-        filmes.pop(indice)
-        session['filmes'] = filmes
-
-def contar_filmes():
-    return len(obter_filmes())
-
-@app.route('/')
-# app.py
 @app.route('/', methods=['GET', 'POST'])
 def filmes():
     if request.method == 'POST':
-        titulo = request.form.get('titulo')
-        if titulo:
-            adicionar_filme(titulo)
-            flash(f'"{titulo}" adicionado com sucesso!', 'success')
-            return redirect(url_for('certo'))
-    meus_filmes = obter_filmes()
-    quantidade = contar_filmes()
-    
-    return render_template('index.html', catalogo=app.config['FILMES'], meus_filmes=meus_filmes, quantidade=quantidade)
+        filme_id = int(request.form.get('filme_id'))
+
+        watchlist = session.get('watchlist', [])
+
+        if filme_id not in watchlist:
+            watchlist.append(filme_id)
+            session['watchlist'] = watchlist
+            flash(
+                f'"{app.config["FILMES"][filme_id]["titulo"]}" adicionado com sucesso!',
+                'success'
+            )
+
+        return redirect(url_for('certo'))
+
+    watchlist_ids = session.get('watchlist', [])
+    filmes_watchlist = []
+    duracao_total = 0
+
+    for filme_id in watchlist_ids:
+        if filme_id in app.config['FILMES']:
+            filme = app.config['FILMES'][filme_id]
+            filmes_watchlist.append(filme)
+            duracao_total += filme['duracao']
+
+    return render_template(
+        'index.html',
+        filmes=app.config['FILMES'],
+        filmes_watchlist=filmes_watchlist,
+        duracao_total=duracao_total
+    )
+
 
 @app.route('/sucesso')
 def certo():
@@ -52,8 +55,6 @@ def watchlist():
     watchlist_ids = session.get('watchlist', [])
     filmes_watchlist = []
     duracao_total = 0
-    
-    # Monta a lista de filmes baseada nos IDs salvos na sessão
     for filme_id in watchlist_ids:
         if filme_id in app.config['FILMES']:
             filme = app.config['FILMES'][filme_id]
