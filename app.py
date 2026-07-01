8a6a2b70f502c627770ea6379e80e3ac0d1386e3
FelipeBlossom <flor.felipe@academico.ifpb.edu.br>
2026-07-01 20:00:56 +0000

    feat: enviar filmes na rota e configuracao

--- app.py
+++ app.py
@@ -9,10 +9,27 @@ FILMES = {
     3: {'id': 3, 'titulo': 'Vingadores: Guerra Infinita',           'ano': 2019, 'duracao': 132},
     4: {'id': 4, 'titulo': 'Miraculous Ladybug Londres',     'ano': 2022, 'duracao': 130},
 }
+app.config['FILMES'] = FILMES
 
 @app.route('/')
 def filmes():
-    return render_template('index.html')
+    return render_template('index.html', filmes=app.config['FILMES'])
+
+# Rota para watchlist
+@app.route('/watchlist')
+def watchlist():
+    watchlist_ids = session.get('watchlist', [])
+    filmes_watchlist = []
+    duracao_total = 0
+    
+    # Monta a lista de filmes baseada nos IDs salvos na sessão
+    for filme_id in watchlist_ids:
+        if filme_id in app.config['FILMES']:
+            filme = app.config['FILMES'][filme_id]
+            filmes_watchlist.append(filme)
+            duracao_total += filme['duracao']
+            
+    return render_template('index.html', filmes=filmes_watchlist, duracao_total=duracao_total)
 
 if __name__ == '__main__':
     app.run(debug=True)
